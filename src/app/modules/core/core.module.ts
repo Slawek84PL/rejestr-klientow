b19f91fc93f7bf466bbd7a046910bfc971ded719
Slawek84PL <slapyt84@gmail.com>
2024-10-08 20:25:17 +0200

    Wszystkie importy z Material zostały imporowane w MaterialModule i exportowane. Dzięki temu możemy importować MaterialModule w CoreModule i mieć dostęp do wszystkich zamieszczonych tam importów.

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MaterialModule } from '../shared/material/material.module';
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterLink],
+  imports: [CommonModule, MaterialModule, RouterLink],
 })
 export class CoreModule {}
