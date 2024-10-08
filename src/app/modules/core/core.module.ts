338b1c3755fb048211d0b4e8731d405029245e09
Slawek84PL <slapyt84@gmail.com>
2024-10-08 20:18:40 +0200

    Dodałem header

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterLink],
 })
 export class CoreModule {}
