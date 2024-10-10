bc83c1c6e940edde3006e64152037998ebffbfb5
Slawek84PL <slapyt84@gmail.com>
2024-10-10 05:46:41 +0200

    Dodałem routing do widoku z logowaniem oraz export componentu logowania i import modułu Auth w app.module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,15 +5,9 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, AuthRoutingModule],
+  exports: [LoginComponent],
 })
-export class AuthModule { }
+export class AuthModule {}
